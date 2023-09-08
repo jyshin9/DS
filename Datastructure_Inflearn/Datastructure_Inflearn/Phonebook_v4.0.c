44bb59f0827b85f7af738ebfc66f8e7bc52bf7c6
jyshin9 <baekju092@gmail.com>
2023-09-08 18:02:58 +0900

    금요일Commit

--- Datastructure_Inflearn/Datastructure_Inflearn/Phonebook_v4.0.c
+++ Datastructure_Inflearn/Datastructure_Inflearn/Phonebook_v4.0.c
@@ -1,1 +1,13 @@
 //phonebook04 | 더 많은 항목, 구조체(structure)
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#define CAPACITY 100
+
+typedef struct Phonebook {
+	char* number;
+	char* email;
+	char* group;
+}phonebook;
+
