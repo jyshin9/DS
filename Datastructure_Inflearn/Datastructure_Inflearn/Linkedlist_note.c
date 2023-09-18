c1367b1f0f9f3a8e05fbbf6deee34a2a899c47bc
Juyoung Shin <baekju092@gmail.com>
2023-09-18 17:59:48 +0900

    Commit

--- Datastructure_Inflearn/Datastructure_Inflearn/Linkedlist_note.c
+++ Datastructure_Inflearn/Datastructure_Inflearn/Linkedlist_note.c
@@ -5,28 +5,9 @@ typedef struct node {
 	struct node* next;
 }Node;
 
-Node* head = NULL;
-
-int main() {
-	head = (Node*)malloc(sizeof(Node));
-	head->data = "Tom";
-	head->next = NULL;
-
-	Node*p= (Node*)malloc(sizeof(Node));
-	p->data = "Dick";
-	p->next = NULL;
-	head->next = p;
-
-	p->next= (Node*)malloc(sizeof(Node));
-	p->next->data = "Harry";
-	p->next->next = NULL;
-	
-	
-}
-
 //포인터 head가 전역변수가 아닌 경우 1
 //호출 시 'add_first(&head, item_to_store)'
-void add_first(Node **ptr_head, char*item) { //포인터변수 head의 주소를 매개변수로 받는다
+void add_first(Node** ptr_head, char* item) { //포인터변수 head의 주소를 매개변수로 받는다
 	Node* tmp = (Node*)malloc(sizeof(Node));
 	tmp->data = item;
 	tmp->next = *ptr_head;
@@ -41,3 +22,18 @@ Node* add_first(Node* head, char* item) { //포인터변수 head의 주소를 매개변수로 
 	tmp->next = head;
 	return tmp; //새로운 head노드의 주소를 return
 }
+
+//어떤 노드 뒤에 새로운 노드 삽입하기
+//성공하면 1, 아니면 0
+int add_after(Node* prev, char* item) { //prev: 삽입할 부분 바로 앞노드
+	if (prev == NULL)
+		return 0;
+	Node* tmp = (Node*)malloc(sizeof(Node));
+	tmp->data = item;
+	tmp->next = prev->next;
+	prev->next = tmp;
+
+	return 1;
+}
+
+//첫번째 노드의 삭제
